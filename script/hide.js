f7d93654b117e62750ba47363162a3c9fe7e0e0e
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-03-16 16:15:29 +0100

    mobile menu

---
+++ script/hide.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector("#nav");
+const button = document.querySelector("#navButton");
+
+
+button.addEventListener("click", function(){
+    if(nav.classList.contains("display")==false && nav.classList.contains("hide")==false ){
+        nav.classList.add("display");
+        return;
+    }
+    else if (nav.classList.contains("display")==true && nav.classList.contains("hide")==false) {
+        nav.classList.remove("display");
+        nav.classList.add("hide");
+        setTimeout(()=>{
+            nav.classList.remove("hide");
+        }, 1000);
+        return;
+    } 
+})
