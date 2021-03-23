24496c334acfa2f132caa0e35317bc381268c07b
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-03-23 10:53:28 +0100

    info section

--- script/szyfr.js
+++ script/szyfr.js
@@ -1,33 +1,43 @@
 const btnPrev = document.querySelector("#btnBefore");
 const btnNext = document.querySelector("#btnNext");
+const firstArticle = document.querySelector(".info");
 let i = 0;
-const slides = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const slides = [firstArticle,2 ,2, ,2, 2, ,2 ,2, 2];
 
 btnNext.addEventListener("click", function(){
-    
+
     if((i >= 0) != (i == slides.length - 1)){
         btnNext.disabled = false;
         btnPrev.disabled = false;
         i += 1;
-    }else if(i == slides.length - 1){
+    }
+    else if(i == slides.length - 1){
         btnNext.disabled = true;
     }
-    console.log(i);
+
+    console.log(i)
+    //showNext(i);
 })
 
 //done
 btnPrev.addEventListener("click", function(){
-    
     if(i == 0){
         btnPrev.disabled = true;
-    }else if(i > 0){
+    }
+    else if(i > 0){
         btnPrev.disabled = false;
         btnNext.disabled = false;
         i -= 1;
     }
-
-    console.log(i);
+    console.log(i)
+    //show(i);
 })
 
-
+function showNext(index){
+    const display = slides[i];
+    const minusDisplay = slides[i - 1];
+    minusDisplay.style.display = "none";
+    display.style.display = "flex";
+}
 
