7c93e84135e08c2f58e80c2207bad07341b43232
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-03-30 12:57:50 +0200

    css edit

--- script/whatGSAP.js
+++ script/whatGSAP.js
@@ -4,18 +4,17 @@ const item = document.querySelectorAll('.main__item');
 
 item.forEach(item => {
     gsap.fromTo(item.children, {
-        y:"+=100",
+        y:"-=200",
         opacity: 0
     },
     {
         y: 0,
         opacity: 1,
-        stagger: 0.2,
         duration: 1,
         scrollTrigger: {
             trigger: item,
-            start: 'top 100%',
-            scrub: 1,
+            start: 'top 120%',
+            scrub: 0,
         }
     })
 })
