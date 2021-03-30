682cca6a9ea8a3d2e4f0b559216886efb95bf59b
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-03-30 15:15:20 +0200

    last updates

--- script/szyfr.js
+++ script/szyfr.js
@@ -6,56 +6,69 @@ const thirdArticle = document.querySelector(".public");
 const fourthArticle = document.querySelector(".publicEncry");
 const fifthArticle = document.querySelector(".privKey");
 const sixthArticle = document.querySelector(".decryption");
+const main = document.querySelector(".main");
 
 let i = 0;
 
-const slides = [firstArticle, secondArticle , thirdArticle, fourthArticle, fifthArticle, sixthArticle];
+const slides = [
+  firstArticle,
+  secondArticle,
+  thirdArticle,
+  fourthArticle,
+  fifthArticle,
+  sixthArticle,
+];
 
-btnNext.addEventListener("click", function(){
-    if((i >= 0) != (i == slides.length - 1)){
-        btnNext.disabled = false;
-        btnPrev.disabled = false;
-        btnPrev.style.display = "block";
-        i += 1;
-    }
-    else if(i == slides.length - 1){
-        btnNext.disabled = true;
-    }
-    window.scrollTo(0, 0);
-    showNext(i);
-})
+btnNext.addEventListener("click", function () {
+  if (i >= 0 != (i == slides.length - 1)) {
+    btnNext.disabled = false;
+    btnPrev.disabled = false;
+    btnPrev.style.display = "block";
+    i += 1;
+  } else if (i == slides.length - 1) {
+    btnNext.disabled = true;
+  }
+  window.scrollTo(0, 0);
+  showNext(i);
+});
 //done
-btnPrev.addEventListener("click", function(){
-    if(i == 0){
-        btnPrev.disabled = true;
-    }
-    else if(i > 0){
-        btnNext.style.display = "block";
-        btnPrev.disabled = false;
-        btnNext.disabled = false;
-        i -= 1;
-    }
-    window.scrollTo(0, 0);
-    showPrev(i);
-})
+btnPrev.addEventListener("click", function () {
+  if (i == 0) {
+    btnPrev.disabled = true;
+  } else if (i > 0) {
+    btnNext.style.display = "block";
+    btnPrev.disabled = false;
+    btnNext.disabled = false;
+    i -= 1;
+  }
+  window.scrollTo(0, 0);
+  showPrev(i);
+});
 
-function showNext(index){
-    const display = slides[i];
-    const minusDisplay = slides[i - 1];
+function showNext(index) {
+  const display = slides[i];
+  const minusDisplay = slides[i - 1];
+  minusDisplay.style.animation = "sliderIn 1s";
+  setTimeout(() => {
     minusDisplay.style.display = "none";
     display.style.display = "flex";
-    if(slides.length - 1 == i){
-        btnNext.style.display = "none";
-    }
+  }, 1000);
+
+  if (slides.length - 1 == i) {
+    btnNext.style.display = "none";
+  }
 }
 
-function showPrev(index){
-    const display = slides[i];
-    const minusDisplay = slides[i + 1];
+function showPrev(index) {
+  const display = slides[i];
+  const minusDisplay = slides[i + 1];
+  display.style.animation = "sliderOut 1s";
+  setTimeout(() => {
     minusDisplay.style.display = "none";
     display.style.display = "flex";
-    if(0 == i){
-        btnPrev.style.display = "none";
-    }
-}
+  }, 1000);
 
+  if (0 == i) {
+    btnPrev.style.display = "none";
+  }
+}
